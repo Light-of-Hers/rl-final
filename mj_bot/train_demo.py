c101e0d2dc63b77b8969eb8b9290a7b73a28703f
Light-of-Hers <724610746@qq.com>
2020-12-21 15:54:40 +0800

    update train_demo.py

--- mj_bot/train_demo.py
+++ mj_bot/train_demo.py
@@ -1,4 +1,4 @@
-from mahjong.common import GameState
+from mahjong.common import GameState, Player
 
 
 class Trainer:
@@ -6,8 +6,16 @@ class Trainer:
     def __init__(self):
         pass
 
-    def process_state_and_action(self, game_state, action):
-        print(action)
+    def print_player(self, player: Player):
+        print("player_id:", player.player_id)
+        print("melds:", [m.cards for m in player.melds])
+        print("n_flowers:", player.n_flowers)
+
+    def handle_state_and_action(self, game_state: GameState, action):
+        print("my_player_id:", game_state.my_pid)
+        print("my_hand:", game_state.my_hand)
+        [self.print_player(p) for p in game_state.players]
+        print("action:", action)
 
 
 def main():
@@ -15,7 +23,7 @@ def main():
     trainer = Trainer()
 
     game_state.load_replay(list(open("../data/sample1.txt", "r")),
-                           callback=trainer.process_state_and_action)
+                           callback=trainer.handle_state_and_action)
 
 
 if __name__ == "__main__":
